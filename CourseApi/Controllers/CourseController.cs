df510a87cb9492c688a54d31ece075d3cd0c1367
Triệu Tuấn Tú <tuantu120502@gmail.com>
2024-11-10 16:01:39 +0000

    init project

---
+++ CourseApi/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello There!");
+        }
+
+    }
+}
